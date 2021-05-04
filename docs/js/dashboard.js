c96e68bd47381d74dc4006fa4859488bad75aa94
soobin99 <tn12qls@naver.com>
2021-05-04 13:01:51 +0900

    폴더정리

--- docs/js/dashboard.js
+++ docs/js/dashboard.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
-    document.getElementById("MarketCapitalizationTable").innerHTML='<object type="text/html" id = "MarketCapitalization" data="MarketCapitalization.html"></object>';
-    document.getElementById("exchangeRateTable").innerHTML='<object type="text/html" id = "exchangeRate" data="exchangeRate.html"></object>';
-    document.getElementById("nationMarketRateTable").innerHTML='<object type="text/html" id = "nationMarketRate" data="nationMarketRate.html"></object>';
-    document.getElementById("goldOilTable").innerHTML='<object type="text/html" id = "goldOil" data="goldOil.html"></object>';
+    document.getElementById("MarketCapitalizationTable").innerHTML='<object type="text/html" id = "innerTable" data="MarketCapitalization.html"></object>';
+    document.getElementById("exchangeRateTable").innerHTML='<object type="text/html" id = "innerTable" data="exchangeRate.html"></object>';
+    document.getElementById("nationMarketRateTable").innerHTML='<object type="text/html" id = "innerTable" data="nationMarketRate.html"></object>';
+    document.getElementById("goldOilTable").innerHTML='<object type="text/html" id = "innerTable" data="goldOil.html"></object>';
+    document.getElementById("interestRateTable").innerHTML='<object type="text/html" id = "innerTable" data="interestRate.html"></object>';
 })
